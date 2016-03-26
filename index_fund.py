6cf4e7260ada626288535d55e6ebbad29ebabf5e
David Karapetyan <david.karapetyan@gmail.com>
2016-03-26 17:41:45 -0400

    expansion of index_fund

--- index_fund.py
+++ index_fund.py
@@ -1,13 +1,61 @@
 from flask import redirect, Flask, request, render_template, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+import wtforms
+from wtforms import StringField, \
+    PasswordField, BooleanField
+from wtforms.validators import DataRequired
+from wtforms.validators import Length, Email
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
-app.config.from_pyfile('default.cfg')
+
+def create_app():
+    _app = Flask(__name__, static_folder='static', template_folder='templates')
+    _app.config.from_pyfile('default.cfg')
+    Bootstrap(_app)
+    return _app
 
 
-# @application.route('/signup')
-# def signup():
-#     return render_template('signup.html', page_title = 'Signup to Bio
-# Application')
+app = create_app()
+
+
+class SignupForm(Form):
+    def generate_csrf_token(self, csrf_context):
+        pass
+
+    email = StringField('Email address',
+                        validators=[
+                            DataRequired(
+                                'Please provide a valid email address'),
+                            Length(min=6, message=u'Email address too short'),
+                            Email(
+                                message=u'That\'s not a valid email address.')])
+    password = PasswordField('Pick a secure password',
+                             validators=[
+                                 DataRequired(),
+                                 Length(min=6,
+                                        message='Please give a longer '
+                                                'password')])
+    username = StringField('Choose your username',
+                           validators=[DataRequired()])
+    agree = BooleanField('I agree to all the Terms of Services',
+                         validators=[
+                             DataRequired(
+                                 u'You must accept our Terms of Service')])
+    submit = wtforms.SubmitField("Submit")
+
+
+@app.route('/signup', methods=("GET", "POST"))
+def signup():
+    return render_template('layout.html', form=SignupForm(),
+                           page_to_insert="signup.html")
+
+
+def submit():
+    form = SignupForm()
+    if form.validate_on_submit():
+        return redirect(url_for("success"))
+    return render_template("signup.html")
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():

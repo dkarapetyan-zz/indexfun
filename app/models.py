7a60be4242aea8c08ac18922d4119cdd25a41cc9
David Karapetyan <david.karapetyan@gmail.com>
2016-04-07 22:47:37 -0400

    up to sending confirmation emails

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
+from flask import current_app
 
 from . import db
 
@@ -19,6 +22,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
+    confirmed = db.Column(db.Boolean, default=False)
 
     def __init__(self, username=None, password=None, email=None):
         self.username = username
@@ -35,6 +39,22 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username

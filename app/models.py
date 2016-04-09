a60f17c730a950d6c28c50231a1a01ff0845ad9f
David Karapetyan <david.karapetyan@gmail.com>
2016-04-09 13:47:43 -0400

    manual implementation of flask-login methods, instead of using mixin

--- app/models.py
+++ app/models.py
@@ -1,10 +1,8 @@
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask.ext.login import UserMixin
 from flask import current_app
 
-from . import db
+from . import db, login_manager
 
 
 class Role(db.Model):
@@ -16,7 +14,7 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
@@ -56,5 +54,32 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    #### for flask-login
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+# for flask-login
+@login_manager.user_loader
+def user_loader(user_id):
+    """Given *user_id*, return the associated User object.
+
+    :param unicode user_id: user_id (email) user to retrieve
+    """
+    return User.query.get(user_id)

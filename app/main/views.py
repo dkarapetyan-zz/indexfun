a8454ca698150eb6b133ed68e45652633b9dc778
David Karapetyan <david.karapetyan@gmail.com>
2016-04-16 19:11:31 -0400

    automatically redirect to login page if user is not logged in and is accessing sensitive pages

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,24 @@
 from flask import render_template, redirect, url_for, request
+from flask.ext.login import login_required, LoginManager
 
 from . import main
 
 __author__ = 'davidkarapetyan'
 
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message = "Please log in to access this page."
+login_manager.login_message_category = "info"
+
 
 @main.route('/', methods=["GET", "POST"])
+@login_required
 def index():
     return render_template('layout.html', page_to_insert="index.html")
 
 
 @main.route("/liquid", methods=["GET", "POST"])
+@login_required
 def liquid():
     # filled = request.form.getlist("check")
     if request.method == "POST":
@@ -19,6 +27,7 @@ def liquid():
 
 
 @main.route("/bank", methods=["GET", "POST"])
+@login_required
 def bank():
     if request.method == "POST":
         return redirect(url_for("main.recurring"))
@@ -26,6 +35,7 @@ def bank():
 
 
 @main.route("/recurring", methods=["GET", "POST"])
+@login_required
 def recurring():
     if request.method == "POST":
         return redirect(url_for("main.index"))
--- config.py
+++ config.py
@@ -27,9 +27,8 @@ class DevelopmentConfig(Config):
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,
-                                                           'data-dev.sqlite')
+    DEV_DATABASE_URI = os.environ.get(
+        'DEV_DATABASE_URI')
 
 
 class TestingConfig(Config):

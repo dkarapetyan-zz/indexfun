b9f33c57aee0e44ebb4b91a1bb9549a00b17000f
David Karapetyan <david.karapetyan@gmail.com>
2016-04-17 17:03:10 -0400

    error handling is working now--got rid of active tab macro

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,10 +4,15 @@ from . import main
 
 
 @main.app_errorhandler(404)
-def page_not_found():
+def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@main.app_errorhandler(405)
+def page_not_found(e):
+    return render_template('405.html'), 405
+
+
 @main.app_errorhandler(500)
-def internal_server_error():
+def internal_server_error(e):
     return render_template('500.html'), 500

62c60fa4df947eaabbc85d17b7491ece60a2b98c
David Karapetyan <david.karapetyan@gmail.com>
2016-04-17 22:46:44 -0400

    got message flashing to work

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
-from flask import render_template, redirect, request, url_for, flash, abort
-from flask.ext.login import login_user, logout_user, login_required, \
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, \
     current_user
 
 from . import auth
@@ -17,10 +17,9 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(url_for('main.index'))
         else:
             flash('Invalid username or password.')
-            return abort(400)
     return render_template('auth/login.html', form=form)
 
 

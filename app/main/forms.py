6d9df46f4a6312e7169ad952626c48a239b3dd3e
David Karapetyan <david.karapetyan@gmail.com>
2016-04-18 17:30:24 -0400

    all pages now use form validators, via wtf forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,17 @@
 __author__ = 'davidkarapetyan'
 from flask_wtf import Form
-from wtforms import FloatField
+from wtforms import FloatField, BooleanField
 
 from wtforms.validators import number_range
 
 
 class Invest(Form):
     money = FloatField('$Amount', validators=number_range(min=0))
+
+
+class Liquid(Form):
+    is_liquid = BooleanField("Make me liquid.")
+
+
+class AutoInvest(Form):
+    is_liquid = BooleanField("Make me AutoInvest.")

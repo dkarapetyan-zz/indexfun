c501034564daa122d3ba1d8bd78cc845430f549b
David Karapetyan <david.karapetyan@gmail.com>
2016-04-24 23:16:30 -0400

    fleshed out bank form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,9 @@
-__author__ = 'davidkarapetyan'
 from flask_wtf import Form
-from wtforms import FloatField, BooleanField, SubmitField
+from wtforms import FloatField, BooleanField, SubmitField, StringField
 
-from wtforms.validators import number_range
+from wtforms.validators import number_range, DataRequired, Length, Regexp
+
+__author__ = 'davidkarapetyan'
 
 
 class Invest(Form):
@@ -21,4 +22,21 @@ class AutoInvest(Form):
 
 
 class BankInfo(Form):
+    StringField("Bank Name", validators=[
+        DataRequired(), Length(1, 64), Regexp('^[A-Za-z\s]*$', 0,
+                                              'Usernames must have only '
+                                              'letters and spaces. '
+                                              )])
+    FloatField("Account Number",
+               validators=[DataRequired(),
+                           Length(1, 17, "Please enter an account number."
+                                         " It should be between 1 and 17 digits")])
+    FloatField("Routing Number",
+               validators=[
+                   DataRequired(), Length(9, 9, message="Routing numbers "
+                                                        "are 9 digits long. "
+                                                        "Please check to make "
+                                                        "sure you have entered "
+                                                        "all 9 digits.")])
+
     submit = SubmitField('Submit')
